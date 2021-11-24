f59ebfd32ea59baa4bf103e44421ec00a19802e6
JayKearney <indi.mh@gmail.com>
2021-11-24 17:29:55 +0000

    change board to dictionary and created player input variable to choose from

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-import random
+
+from random import randint
 import sys
 
+    
+#Create a dictionary
+
+
+def printBoard(board):
+    print(board['top-left'] + '|'+board['top-center']+'|'+board['top-right'])
+    print('-+-+-')
+    print(board['mid-left'] + '|'+board['mid-center']+'|' + board['mid-right'])
+    print('-+-+-')
+    print(board['lower-left'] + '|'+board['lower-center']+'|' + board['lower-right'])
+
+def player_input():
+
+    value = ""
+
+    while not (value == "X" or value == "O"):
+        value = input("Please pick a value 'X' or 'O': ")
+    return (value)
